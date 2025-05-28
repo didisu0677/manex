139b5132c7bbfebaf1a8ef95febb0ca0eeaf2afe
didisu0677 <didisu0677@gmail.com>
2025-05-28 08:37:08 +0700

    total rencana pemakaian

--- application/admin/material_cost/views/rencana_pemakaian/table.php
+++ application/admin/material_cost/views/rencana_pemakaian/table.php
@@ -17,15 +17,16 @@
 			$bgedit ="";
 			$contentedit ="true" ;
 			// for ($i = setting('actual_budget'); $i <= 12; $i++) {
+			$total = 0;
 			for ($i = 1; $i <= 12; $i++) {
 
 				$field0 = 'P_' . sprintf('%02d', $i);
-
+				$total += $m1[$field0];
 
 				echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget '.$field0.'" data-name="'.$field0.'" data-id="" data-value="">'.number_format($m1[$field0]).'</td>';
 			
 			}
-			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right calculate total_est" data-name="" data-id="'.$m1->id.'" data-value=""><b></b></td>';
+			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right calculate total_est" data-name="" data-id="'.$m1->id.'" data-value=""><b>'.number_format($total).'</b></td>';
 
 			?>
 
