12753c964f0d1eedd4ec55cb176ad61fa43fc2ff
didisu0677 <didisu0677@gmail.com>
2025-05-28 14:09:29 +0700

    cogs

--- application/helpers/ebudget_helper.php
+++ application/helpers/ebudget_helper.php
@@ -958,9 +958,11 @@ function recalculate_sales($tahun="",$product="",$sector="") {
 
 
 				update_data($table7,[$fest_cogs=>$sales_amount->$fest_cogs, $fbud_cogs => $sales_amount->$fbud_cogs1, $fthn_cogs => $sales_amount->$fthn_cogs1],
-				['budget_product_code1'=>$product,'tahun'=>$tahun,'budget_product_sector'=>$sector]); 
+				['budget_product_code'=>$product,'tahun'=>$tahun,'budget_product_sector'=>$sector]); 
 
 			}
+
+			debug($sales_amount);die;
 
 		// update_data($table7,
 		// 	['EST_01' => $sales_amount->CEST_01,'EST_02' => $sales_amount->CEST_02,'EST_03' => $sales_amount->CEST_03,'EST_04' => $sales_amount->CEST_04,
