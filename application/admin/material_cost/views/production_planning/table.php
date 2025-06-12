5e8c0612f211fb5d2939d834a8a48ab564062070
didisu0677 <didisu0677@gmail.com>
2025-06-12 14:11:16 +0700

    asdf

--- application/admin/material_cost/views/production_planning/table.php
+++ application/admin/material_cost/views/production_planning/table.php
@@ -501,7 +501,12 @@
 	$contentedit ="false" ;
 	// for ($i = setting('actual_budget'); $i <= 12; $i++) {
 	for ($i = 1; $i <= 12; $i++) {			
+		$gt_begining = 'tbegining' . sprintf('%02d', $i);
+		$gt_prod = 'tprod_' . sprintf('%02d', $i);
+		$gt_sales = 'tsales' . sprintf('%02d', $i);
 		$gt_end = 'tend' . sprintf('%02d', $i);
+
+		$$gt_end = ($$gt_begining + $$gt_prod) - $$gt_sales ;
 
 		$sumtotalfield0 = 'sumTotalB_' . sprintf('%02d', $i);
 		echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget " data-name="" data-id="'.$m1->id.'" data-value="'.$$sumtotalfield0.'"><b>'.number_format($$gt_end).'</b></td>';
