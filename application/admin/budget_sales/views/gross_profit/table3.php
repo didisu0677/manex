76ae684fb7172a7c55514c600cc639d320c75c62
didisu0677 <didisu0677@gmail.com>
2025-06-19 08:44:16 +0700

    table 3

--- application/admin/budget_sales/views/gross_profit/table3.php
+++ application/admin/budget_sales/views/gross_profit/table3.php
@@ -5,6 +5,10 @@
 		$totalfield0 = 'TotalTHN_' . sprintf('%02d', $i);
 		$$totalfield0 = 0;
 	}
+
+	$total_actual = 0;
+	$total_curbudget = 0;
+
 	foreach($grup[0] as $m0) { ?>
 		<tr>
             <?php $colspan = 15; ?>
@@ -12,16 +16,22 @@
 		</tr>		
   	<?php
 
+	for ($i = 1; $i <= 10; $i++) {
+		$sub_totalfield0 = 'sub_TotalTHN_' . sprintf('%02d', $i);
+		$$sub_totalfield0 = 0;
+	}
+
+	$sub_total_actual = 0;
+	$sub_total_curbudget = 0;
+
 	foreach($produk[$m0->product_line] as $m2 => $m1) { 
 		// debug($m1->product_name);die;
 		// debug(isset($m1['product_name']) ? $m1['product_name'] : '');die;
-			$no++;
 						
 		$bgedit ="";
 		$contentedit ="false" ;
 		?>
 		<tr>
-
 			<td><?php echo isset($m1->product_name) ? $m1->product_name : ''; ?></td>
 			<td><?php echo isset($m1->code) ? $m1->code : ''; ?></td>
 			<td><?php echo isset($m1->segment) ? $m1->segment : ''; ?></td>
@@ -29,16 +39,33 @@
 
 			$bgedit ="";
 			$contentedit ="false" ;
-			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""></td>';
-			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""></td>';
+
+			$actual = 0;
+			for($i = 1; $i <= 12 ; $i++) { //setting('actual_budget');
+				$field_actual = 'EST_' . sprintf('%02d', $i);
+				$actual += $m1->$field_actual;
+			}
+
+			$total_actual += $actual ;
+			$sub_total_actual += $actual ;
+
+			$total_curbudget += $m1->total_budget;
+			$sub_total_curbudget += $m1->total_budget;
+
+			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value="">'.number_format($actual).'</td>';
+			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value="">'.number_format($m1->total_budget).'</td>';
 
 			for ($i = 1; $i <= 10; $i++) {
 				$field0 = 'THN_' . sprintf('%02d', $i);
+
+				$sub_totalfield0 = 'sub_TotalTHN_' . sprintf('%02d', $i);
+				$$sub_totalfield0 += $m1->$field0;
+
 				$totalfield0 = 'TotalTHN_' . sprintf('%02d', $i);
 				$$totalfield0 += $m1->$field0;
 				
 				$x1 = ($contentedit == 'true' ? number_format($m1->$field0) : '');
-				echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget '.$field0.'" data-name="'.$field0.'" data-id="'.$m1->id.'" data-value="'.$x1.'">'.$x1.'</td>';
+				echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget '.$field0.'" data-name="'.$field0.'" data-id="'.$m1->id.'" data-value="'.$x1.'">'.number_format($m1->$field0).'</td>';
 			}
             // echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right calculate total_budgetthn" data-name="total_budgetthn" data-id="'.$m1->id.'" data-value=""></td>';
 
@@ -52,13 +79,27 @@
 		<?php
 			$bgedit ="";
 			$contentedit = "false"; 
-			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""></td>';
-			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""></td>';
+			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""><b>'.number_format($sub_total_actual).'</b></td>';
+			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""><b>'.number_format($sub_total_curbudget).'</b></td>';
 
 			for ($i = 1; $i <= 10; $i++) {
 				$totalfield0 = 'TotalTHN_' . sprintf('%02d', $i);
-				echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget " data-name="" data-id="'.$m1->id.'" data-value="'.$$totalfield0.'">'.number_format($$totalfield0).'</td>';
+				echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget " data-name="" data-id="'.$m1->id.'" data-value="'.$$sub_totalfield0.'"><b>'.number_format($$sub_totalfield0).'</b></td>';
 			}
 			?>
 	</tr
 <?php } ?>
+<tr>
+	<td class="sub-1" colspan="3"><b>GRAND TOTAL </b></td>
+	<?php
+		$bgedit ="";
+		$contentedit = "false"; 
+		echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""><b>'.number_format($total_actual).'</b></td>';
+		echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget actual" data-name="actual" data-id="'.$m1->id.'" data-value=""><b>'.number_format($total_curbudget).'</b></td>';
+
+		for ($i = 1; $i <= 10; $i++) {
+			$totalfield0 = 'TotalTHN_' . sprintf('%02d', $i);
+			echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget " data-name="" data-id="'.$m1->id.'" data-value="'.$$totalfield0.'"><b>'.number_format($$totalfield0).'<b></td>';
+		}
+		?>
+</tr
