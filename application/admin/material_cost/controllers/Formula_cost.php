2dfdba099819619a2f7bb1c20f9b86cb43748c03
didisu0677 <didisu0677@gmail.com>
2025-06-23 13:01:24 +0700

    commit

--- application/admin/material_cost/controllers/Formula_cost.php
+++ application/admin/material_cost/controllers/Formula_cost.php
@@ -62,7 +62,7 @@ class Formula_cost extends BE_Controller {
 
         //
 
-        $data['detail']	= get_data('tbl_material_formula1 a',[
+        $data['detail']	= get_data('tbl_material_formula a',[
             'select'	=> 'a.parent_item, a.component_item, a.material_name, 
                             a.quantity, a.um, a.group_formula, b.bm, 
                             b.bank_charges, b.handling_charges, b.price_us ,b.curr, c.rates, c.ppn, c.pph, (b.price_us * c.rates) as total_price',
