b552083d095b9c77ebe10e0642c44357add05a84
didisu0677 <didisu0677@gmail.com>
2025-06-23 13:01:17 +0700

    formula cost

--- application/admin/material_cost/controllers/Formula_cost.php
+++ application/admin/material_cost/controllers/Formula_cost.php
@@ -48,7 +48,7 @@ class Formula_cost extends BE_Controller {
         $data['produk']	= get_data('tbl_material_formula a',[
             'select'	=> 'a.parent_item, a.component_item, a.material_name, 
                             a.quantity, a.um, a.group_formula', 
-            'join' => ['tbl_material_price b on a.component_item = b.material_code type LEFT and b.year="'.$tahun.'"',
+            'join' => ['tbl_material_price b on a.component_item = b.material_code and b.year="'.$tahun.'" type LEFT',
                     'tbl_currency_rates c on b.curr = c.curr type LEFT'
                     ],
             'where'		=> [
@@ -57,16 +57,16 @@ class Formula_cost extends BE_Controller {
                 'a.component_item' => 'CIRMDBBF1J'
                 ],
             'group_by' => 'a.parent_item,a.component_item',
-            'sort_by' => 'a.parent_item'
+            'sort_by' => 'a.parent_item,a.component_item'
         ])->result();
 
-
+        //
 
         $data['detail']	= get_data('tbl_material_formula1 a',[
             'select'	=> 'a.parent_item, a.component_item, a.material_name, 
                             a.quantity, a.um, a.group_formula, b.bm, 
                             b.bank_charges, b.handling_charges, b.price_us ,b.curr, c.rates, c.ppn, c.pph, (b.price_us * c.rates) as total_price',
-            'join' => ['tbl_material_price b on a.component_item = b.material_code type LEFT and b.year="'.$tahun.'"',
+            'join' => ['tbl_material_price b on a.component_item = b.material_code and b.year="'.$tahun.'" type LEFT ',
                     'tbl_currency_rates c on b.curr = c.curr type LEFT'
                     ],
             'where'		=> [
