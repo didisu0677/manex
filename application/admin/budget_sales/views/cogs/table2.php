a119fe187d8ebe871e4b54eeea0ee5e2889fb440
didisu0677 <didisu0677@gmail.com>
2025-09-17 16:08:35 +0700

    total budget

--- application/admin/budget_sales/views/cogs/table2.php
+++ application/admin/budget_sales/views/cogs/table2.php
@@ -52,8 +52,8 @@
 				$x1 = (number_format($m1->$field0));
 				echo '<td style="background: '.$bgedit.'"><div style="background:'.$bgedit.'" style="min-height: 10px; width: 50px; overflow: hidden;" contenteditable="'.$contentedit.'" class="edit-value text-right budget '.$field0.'" data-name="'.$field0.'" data-id="'.$m1->id.'" data-value="'.$x1.'">'.$x1.'</td>';		
 			}
-			
-			$stotal_est = $m1->EST_01+$m1->EST_02+$m1->EST_03+$m1->EST_04+$m1->EST_05+$m1->EST_06+$m1->EST_07+$m1->EST_08+$m1->EST_09+$m1->EST_10+$m1->EST_11+$m1->EST_12;
+
+			$stotal_est = $m1->B_01+$m1->B_02+$m1->B_03+$m1->B_04+$m1->B_05+$m1->B_06+$m1->B_07+$m1->B_08+$m1->B_09+$m1->B_10+$m1->B_11+$m1->B_12;
             $stotal_budget += $stotal_est;
 			$sumstotal_budget += $stotal_est;
 
