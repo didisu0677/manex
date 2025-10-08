511be16d7f635db510181b864898fea76d930163
didisu0677 <didisu0677@gmail.com>
2025-10-08 15:03:38 +0700

    get

--- application/admin/transaction/controllers/Aloc_service_actual.php
+++ application/admin/transaction/controllers/Aloc_service_actual.php
@@ -131,7 +131,7 @@ class Aloc_service_actual extends BE_Controller {
 
             foreach($cc_source as $c) {
                 $sum = get_data($table0 . ' a',[
-                    'select' => 'a.cost_centre1,a.id_cost_centre,a.sub_account,a.account_code,a.id_account,a.account_name,
+                    'select' => 'a.cost_centre,a.id_cost_centre,a.sub_account,a.account_code,a.id_account,a.account_name,
                           sum(a.'.$field.') as "'.$field.'", sum(total_budget) as total_budget',
                      'where' => [
                         'a.cost_centre' => $c,
@@ -140,7 +140,6 @@ class Aloc_service_actual extends BE_Controller {
                     'group_by' => 'a.cost_centre,a.id_cost_centre,a.sub_account,a.account_code,a.id_account'
                 ])->result();   
 
-                debug($sum);die;
                 
                 if(count($sum)) {
                      foreach($sum as $s) {
