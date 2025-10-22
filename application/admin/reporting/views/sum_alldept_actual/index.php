f0634ce13022eb2b80c2b98eb9f338a1862e214c
didisu0677 <didisu0677@gmail.com>
2025-10-22 13:56:16 +0700

    float right

--- application/admin/reporting/views/sum_alldept_actual/index.php
+++ application/admin/reporting/views/sum_alldept_actual/index.php
@@ -166,21 +166,7 @@ body {
     padding: 0 !important;
 }
 
-/* Positioning filter agar sejajar dengan title */
-.content-header .main-container {
-    display: flex !important;
-    justify-content: space-between !important;
-    align-items: flex-start !important;
-}
 
-.content-header .header-info {
-    flex: 1 !important;
-}
-
-.content-header .float-right {
-    margin-top: 0 !important;
-    margin-left: 20px !important;
-}
 
 
 </style>
