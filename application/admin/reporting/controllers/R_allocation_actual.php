4072dccfa1c6b26eeacf0cf806c3c79a6c911dda
didisu0677 <didisu0677@gmail.com>
2025-10-29 16:56:39 +0700

    idle allocation

--- application/admin/reporting/controllers/R_allocation_actual.php
+++ application/admin/reporting/controllers/R_allocation_actual.php
@@ -219,7 +219,7 @@ class R_allocation_actual extends BE_Controller {
         $lst = get_data('tbl_fact_manex_allocation_actual a',[
 			'select' => 'a.*,b.kode as cost_centre,c.prsn_allocation',
 			'join'   => ['tbl_fact_cost_centre b on a.cost_centre = b.kode type LEFT',
-                       'tbl_idle_allocation c on b.id = c.id_cost_centre and b.is_active = 1 type left',
+                       'tbl_idle_allocation_actual c on b.id = c.id_cost_centre and a.bulan = c.bulanand b.is_active = 1 type left',
                         ],
             'where'  => [
                         'a.tahun' => $tahun,
