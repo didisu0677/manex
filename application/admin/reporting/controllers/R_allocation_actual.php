f78c24245394cf546b6fc7b9adad7f05cad3a936
didisu0677 <didisu0677@gmail.com>
2025-10-30 13:28:55 +0700

    idle allocation

--- application/admin/reporting/controllers/R_allocation_actual.php
+++ application/admin/reporting/controllers/R_allocation_actual.php
@@ -233,6 +233,9 @@ class R_allocation_actual extends BE_Controller {
             if(in_array($l->manex_account,['7212','735','736','738','759'])) {
                 $total_idle = ($l->total * ($l->prsn_allocation /100));
                 $after_idle = $l->total - ($l->total * ($l->prsn_allocation /100));
+            }else{
+                $total_idle = 0;
+                $after_idle = $l->total;
             }
 
                 update_data('tbl_fact_manex_allocation_actual',
