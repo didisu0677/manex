94a3edd34d77e385b64b4607c732d7c916e45740
didisu0677 <didisu0677@gmail.com>
2025-12-03 16:01:28 +0700

    Update cookie domain and security settings in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -403,9 +403,8 @@ $config['sess_regenerate_destroy'] = FALSE;
 |
 */
 $config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
+$config['cookie_domain']	= '.dev-manex.otsuka.co.id';
+$config['cookie_secure']	= TRUE;
 $config['cookie_httponly'] 	= FALSE;
 
 /*
