ac5bc7ce12ce3eba4473358b5a237764f40c0020
Faisal S <fsyarifuddin@ho.otsuka.co.id>
2025-12-03 10:41:52 +0700

    Edit config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $_SERVER['HTTPS'] = 'on';
+    $_SERVER['SERVER_PORT'] = 443;
+}
+
 $http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
 $newurl = str_replace('index.php','', $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $http . $_SERVER['SERVER_NAME'] .  $newurl;
